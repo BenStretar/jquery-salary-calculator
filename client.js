6fa16c258a76001ffcb17968c1cfdda461ac55db
Ben Stretar <ben.stretar@gmail.com>
2019-11-08 16:16:46 -0600

    button adds inputs to array

--- client.js
+++ client.js
@@ -2,6 +2,32 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('hello');
-
+    $('#add-employee').on('click', addEmployeesTable)
 }
 
+//make employees array
+const employees =[];
+
+function addEmployeesTable(event){
+    event.preventDefault();
+    let firstName = $('#first-name').val()
+    let lastName = $('#last-name').val()
+    let id = $('#em-id').val()
+    let title = $('#title').val()
+    let annualSalary = $('#annual-salary').val()
+
+    employees.push(
+        {
+            firstName, lastName, id, title, annualSalary
+        }
+    )
+
+    // clear inputs
+    $('#first-name').val('');
+    $('#last-name').val('');
+    $('#em-id').val('');
+    $('#title').val('');
+    $('#annual-salary').val('');
+
+    showEmployees(employees)
+}
