aaf1ae24f978fb5f69b790e403ae92be5ea3be0d
Ben Stretar <ben.stretar@gmail.com>
2019-11-10 14:03:34 -0600

    calculates monthly total and changes background red if over or equal to said amount

--- client.js
+++ client.js
@@ -3,11 +3,14 @@ $(document).ready(onReady);
 function onReady(){
     console.log('hello');
     $('#add-employee').on('click', addEmployeesTable)
+    calculateMonthlyTotal();
+    $('#employee-table').on('click', '.delete', removeEmployee)
 }
 
 //make employees array
 const employees =[];
 
+// take input deom the form and put into employees array
 function addEmployeesTable(event){
     event.preventDefault();
     let firstName = $('#first-name').val()
@@ -29,5 +32,58 @@ function addEmployeesTable(event){
     $('#title').val('');
     $('#annual-salary').val('');
 
-    showEmployees(employees)
+    showEmployees(employees);
+    calculateMonthlyTotal();
+}
+
+// show employees on the DOM
+
+function showEmployees(array){
+    $('#employee-table').empty();
+
+    for (let employees of array) {
+        $('#employee-table').append(`
+        <tr>
+            <td>${employees.firstName}</td>
+            <td>${employees.lastName}</td>
+            <td>${employees.id}</td>
+            <td>${employees.title}</td>
+            <td>${employees.annualSalary}</td>
+            <td><button class='delete'>Delete</button></td>
+        </tr>
+        `);
+    }
+}
+//remove employee from the DOM
+function removeEmployee(){
+   // console.log('removeEmployee');
+
+    let button = $(this);
+    // console.log('button', button);
+
+    button.closest('tr').remove();
+}
+
+
+
+
+function calculateMonthlyTotal(){
+    let totalSal = 0;
+
+    for (let i = 0; i < employees.length; i++){
+        totalSal += Number(employees[i].annualSalary)
+    }// end for
+    // annualSal divided by 12
+    const monthlySal = totalSal / 12;
+
+    // display monthly budget
+    let monthlyBudget = $('#monthlyTotal');
+    monthlyBudget.empty();
+    monthlyBudget.append( monthlySal )
+
+    // change color if over or equal to $20,000
+    if (monthlySal >= 20000){
+        $('#monthlyTotal').addClass('red');
+        $('#monthlyHeader').addClass('red');
+    }
 }
